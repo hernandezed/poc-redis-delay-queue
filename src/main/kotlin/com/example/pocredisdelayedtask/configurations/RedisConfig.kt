e848d7733fd5788495da5c3d8c6deabe82f65d51
Eduardo Hernandez <edhernandez@santandertecnologia.com.ar>
2021-07-22 11:13:03 -0300

    testing travis

--- src/main/kotlin/com/example/pocredisdelayedtask/configurations/RedisConfig.kt
+++ src/main/kotlin/com/example/pocredisdelayedtask/configurations/RedisConfig.kt
@@ -3,14 +3,16 @@ package com.example.pocredisdelayedtask.configurations
 import com.example.pocredisdelayedtask.data_access.redis.entities.DelayedTask
 import com.example.pocredisdelayedtask.data_access.redis.structures.DelayedQueue
 import com.example.pocredisdelayedtask.data_access.redis.structures.impl.DelayedQueueImpl
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration.LettuceClientConfigurationBuilder
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 
+
 @Configuration
 class RedisConfig {
-
     @Bean
     fun delayedTaskQueue(connectionFactory: LettuceConnectionFactory): DelayedQueue<DelayedTask> {
         val template = RedisTemplate<String, DelayedTask>()
@@ -19,4 +21,12 @@ class RedisConfig {
         return DelayedQueueImpl(template, "delayed-task");
     }
 
+    @Bean
+    fun lettuceClientConfigurationBuilderCustomizer(): LettuceClientConfigurationBuilderCustomizer? {
+        return LettuceClientConfigurationBuilderCustomizer { clientConfigurationBuilder: LettuceClientConfigurationBuilder ->
+            if (clientConfigurationBuilder.build().isUseSsl) {
+                clientConfigurationBuilder.useSsl().disablePeerVerification()
+            }
+        }
+    }
 }
